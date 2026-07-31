7b07067eca008042961406b8f1981692bea2ae2d
aicha <chrikaaicha@gmail.com>
2026-07-31 02:11:57 +0100

    Initial Spring Boot project

---
+++ src/main/java/ma/pmp/sjdg/SgdjApplication.java
@@ -0,0 +1,13 @@
+package ma.pmp.sjdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgdjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgdjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/pmp/sjdg/SgdjApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.pmp.sjdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
